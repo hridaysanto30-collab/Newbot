ac4a7c3c2be47938e89bfb6fe3ebd22b15b5e720
mdsiyam121314151-source <mdsiyam121314151@gmail.com>
2026-04-27 21:12:39 +0600

    Update text_voice.js

--- scripts/cmds/text_voice.js
+++ scripts/cmds/text_voice.js
@@ -5,7 +5,7 @@ const path = require("path");
 module.exports = {
   config: {
     name: "text_voice",
-    version: "1.0.5",
+    version: "1.0.6",
     author: "MR_FARHAN", // ⚠️ DO NOT CHANGE THIS (LOCKED)
     countDown: 1,
     role: 0,
@@ -27,7 +27,6 @@ module.exports = {
   onStart: async function () {},
 
   onChat: async function ({ event, message }) {
-    // 🔒 Run lock check every time
     this._authorLock();
 
     if (!event.body) return;
@@ -35,23 +34,40 @@ module.exports = {
     const input = event.body.toLowerCase().trim();
 
     const voiceMap = {
+      // 🔥 OLD রাখা
       "magi": "https://files.catbox.moe/ecgpak.mp4",
       "মাগি": "https://files.catbox.moe/ecgpak.mp4",
-      
+
       "খানকি": "https://files.catbox.moe/ecgpak.mp4",
       "khanki": "https://files.catbox.moe/ecgpak.mp4",
-
-      "farhan": "https://files.catbox.moe/tvpfee.mp3",
-      "ফারহান": "https://files.catbox.moe/tvpfee.mp3",
-
-      "sizuka": "https://files.catbox.moe/3u6shs.mp3",
-      "সিজুকা": "https://files.catbox.moe/3u6shs.mp3",
 
       "good night": "https://files.catbox.moe/i29m4q.mp3",
       "গুড নাইট": "https://files.catbox.moe/i29m4q.mp3",
 
       "good morning": "https://files.catbox.moe/8gzqx5.mp3",
-      "গুড মর্নিং": "https://files.catbox.moe/8gzqx5.mp3"
+      "গুড মর্নিং": "https://files.catbox.moe/8gzqx5.mp3",
+
+      // ✅ NEW ADD
+      "siyam": "https://files.catbox.moe/9w6moo.mp3",
+      "সিয়াম ভাই": "https://files.catbox.moe/9w6moo.mp3",
+      "সিয়াম": "https://files.catbox.moe/9w6moo.mp3",
+
+      "@তো্ঁমা্ঁগো্ঁ পি্ঁচ্ছি্ঁ উ্ঁদয়্ঁ তা্ঁহ": "https://files.catbox.moe/9w6moo.mp3",
+
+      "@everyone": "https://files.catbox.moe/par79o.mp4",
+
+      "নিঝুম": "https://files.catbox.moe/par79o.mp4",
+
+      // 🔥 emoji + trigger
+      ",sex": "https://files.catbox.moe/uy7mrv.mp3",
+      ",hot": "https://files.catbox.moe/m5djca.mp3",
+      "s+n": "https://files.catbox.moe/ztwmnf.mp4",
+      "টুকি": "https://files.catbox.moe/e8ebel.mp3",
+      "আমি মাদিহা": "https://files.catbox.moe/9gyjwp.mp3",
+      "নুনু": "https://files.catbox.moe/r5uz42.mp3",
+
+      "🐍": "https://files.catbox.moe/s1k2nx.mp4",
+      "😈": "https://files.catbox.moe/5rdtc6.mp3"
     };
 
     if (voiceMap[input]) {
